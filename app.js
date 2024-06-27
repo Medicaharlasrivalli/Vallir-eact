19ddcaf290cd327bb21af9f0a84a410914897094
Medicaharlasrivalli <srivallimedicharla@gmail.com>
2024-06-27 11:05:31 -0500

    episode1

---
+++ app.js
@@ -0,0 +1,20 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement(
+    "div",
+    { id: "child1" },
+    [React.createElement("h1", {}, "I am h1 tag"),React.createElement("h2", {}, "I am h2 tag")]
+  ),
+  React.createElement(
+    "div",
+    { id: "child2" },
+    [React.createElement("h1", {}, "I am h1 tag"),React.createElement("h2", {}, "I am h2 tag")]
+  )
+    ]
+);
+
+console.log(parent); //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//render-it is converting the object into a h1 tag.
+root.render(parent);
